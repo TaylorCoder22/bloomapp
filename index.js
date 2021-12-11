e999cd174dbf0a2339d138ee0c387eee4a2b43c0
tamara-taylor <ttaylor8522@gmail.com>
2021-12-11 16:48:32 -0500

    monolith

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path')
 const port = process.env.PORT || 9000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.use('/api/', (_, res) => {
+app.use('/api/*', (_, res) => {
     res.json({data: 'API Data Served!'})
 })
 
 app.listen(port, () => {
     console.log(`Server is on port ${port}`)
+})
+
+app.use('*', (__, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 //console.log("its alive!")
